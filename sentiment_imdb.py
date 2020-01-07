35f77f3483f20688856111d96d69da52c3e33b63
Emanuel Joebstl <emanuel.joebstl@gmail.com>
2020-01-07 21:26:14 +0100

    TF 2.0 compatibility.

--- sentiment_imdb.py
+++ sentiment_imdb.py
@@ -10,7 +10,7 @@ def load_directory_data(directory):
   data["sentence"] = []
   data["sentiment"] = []
   for file_path in os.listdir(directory):
-    with tf.gfile.GFile(os.path.join(directory, file_path), "r") as f:
+    with tf.io.gfile.GFile(os.path.join(directory, file_path), "r") as f:
       data["sentence"].append(f.read())
       data["sentiment"].append(re.match("\d+_(\d+)\.txt", file_path).group(1))
   return pd.DataFrame.from_dict(data)
